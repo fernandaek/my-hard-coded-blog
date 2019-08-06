aba76a002a24b159aa0c28e939408d3a12e5f194
Fernanda Ek <fernandaek@icloud.com>
2019-08-06 19:44:19 +0200

    My first commit

---
+++ Blog/Entities/Post.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        //Most of the time we need add and remove an object so just do it unless its explicit to dont
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(Title);
+            stringBuilder.Append(Likes);
+            stringBuilder.Append(" Likes - " + Moment );
+            stringBuilder.AppendLine(Content);
+            stringBuilder.AppendLine("Comments: ");
+
+            foreach(Comment comment in Comments)
+            {
+                stringBuilder.AppendLine(comment.Text);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Blog/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using Blog.Entities;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment comment_1 = new Comment("Have a nice trip!"); //Hard coded
+            Comment comment_2 = new Comment("Wow that's awesome!");
+
+            Post post_1 = new Post(
+                DateTime.ParseExact("21/06/2019 13:05:44", "dd/MM/yyyy H:mm:ss", null),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12
+                );
+            post_1.AddComment(comment_1);
+            post_1.AddComment(comment_2);
+
+            //Console.WriteLine(comment_1);
+
+            Comment comment_3 = new Comment("Good Night!");
+            Comment comment_4 = new Comment("May the force be with you.");
+
+            Post post_2 = new Post(
+                DateTime.ParseExact("28/07/2019 23:20:14", "dd/MM/yyyy H:mm:ss", null),
+                "Good night guys",
+                "See you tomorrow",
+                5
+                );
+            post_2.AddComment(comment_3);
+            post_2.AddComment(comment_4);
+
+            Console.WriteLine(post_1);
+            Console.WriteLine(post_2);
+        }
+    }
+}
